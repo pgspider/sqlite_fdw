ff232874f174af1e29c12d30285bbf3ab512e7c8
Михаил <aksenov.mihail@bk.ru>
2023-11-02 17:38:13 +0300

    Fix variable ini blocks

--- sqlite_query.c
+++ sqlite_query.c
@@ -149,6 +149,7 @@ sqlite_convert_to_pg(Form_pg_attribute att, sqlite3_stmt * stmt, int stmt_colid,
 					case SQLITE_FLOAT: /* TODO: This code is untill mod() pushdowning fix here*/
 					{
 						int			value = sqlite3_column_int(stmt, stmt_colid);
+
 						elog(DEBUG2, "sqlite_fdw : real aff. was readed for pg int32");
 						return (struct NullableDatum) {Int32GetDatum(value), false};
 					}
@@ -181,9 +182,10 @@ sqlite_convert_to_pg(Form_pg_attribute att, sqlite3_stmt * stmt, int stmt_colid,
 					case SQLITE_FLOAT: /* TODO: This code is untill mod() pushdowning fix here*/
 					{
 						int			value = sqlite3_column_int(stmt, stmt_colid);
+
 						elog(DEBUG2, "sqlite_fdw : real aff. was readed for pg int64");
 						return (struct NullableDatum) {Int32GetDatum(value), false};
-					}					
+					}
 					case SQLITE_BLOB:
 					default:
 						{
@@ -306,6 +308,7 @@ sqlite_convert_to_pg(Form_pg_attribute att, sqlite3_stmt * stmt, int stmt_colid,
 					case SQLITE_FLOAT: /* <-- proper and recommended SQLite affinity of value for pgtyp */
 						{
 							double		value = sqlite3_column_double(stmt, stmt_colid);
+
 							valstr = DatumGetCString(DirectFunctionCall1(float8out, Float8GetDatum((float8) value)));
 							break; /* !!! use valstr later! */
 						}
@@ -349,6 +352,7 @@ sqlite_convert_to_pg(Form_pg_attribute att, sqlite3_stmt * stmt, int stmt_colid,
 							{
 								const unsigned char * sqlite_blob = 0;
 								pg_uuid_t  *retval = (pg_uuid_t *) palloc0(sizeof(pg_uuid_t));
+
 								sqlite_blob = sqlite3_column_blob(stmt, stmt_colid);
 								memcpy(retval->data, sqlite_blob, UUID_LEN);
 								return (struct NullableDatum){UUIDPGetDatum(retval), false};
@@ -374,8 +378,9 @@ sqlite_convert_to_pg(Form_pg_attribute att, sqlite3_stmt * stmt, int stmt_colid,
 		case VARBITOID:
 		case BITOID:
 			{
-				char * buffer = palloc0(SQLITE_FDW_BIT_DATATYPE_BUF_SIZE);			
-				sqlite3_int64 sqlti = sqlite3_column_int64(stmt, stmt_colid);				
+				char * buffer = (char *) palloc0(SQLITE_FDW_BIT_DATATYPE_BUF_SIZE);
+
+				sqlite3_int64 sqlti = sqlite3_column_int64(stmt, stmt_colid);
 				buffer = int642binstr(sqlti, buffer, SQLITE_FDW_BIT_DATATYPE_BUF_SIZE);
 				valstr = buffer;
 				elog(DEBUG4, "sqlite_fdw : BIT buf l=%ld v = %s", SQLITE_FDW_BIT_DATATYPE_BUF_SIZE, buffer);
@@ -569,13 +574,13 @@ sqlite_bind_sql_var(Form_pg_attribute att, int attnum, Datum value, sqlite3_stmt
 
 				if (uuid_as_blob)
 				{
-					unsigned char *dat = palloc0(UUID_LEN);		
+					unsigned char *dat = palloc0(UUID_LEN);
 					pg_uuid_t* pg_uuid = DatumGetUUIDP(value);
 					elog(DEBUG2, "sqlite_fdw : bind uuid as blob");
-					memcpy(dat, pg_uuid->data, UUID_LEN);					
+					memcpy(dat, pg_uuid->data, UUID_LEN);
 					ret = sqlite3_bind_blob(stmt, attnum, dat, UUID_LEN, SQLITE_TRANSIENT);
 				}
-				else 
+				else
 				{
 					/* uuid as text */
 					char	   *outputString = NULL;
