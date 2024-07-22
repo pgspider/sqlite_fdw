95ebc81467025b38b26615b331541dd009eeff72
MinhLA <vagrant@localhost.localdomain>
2024-07-22 14:16:02 +0700

    improve CI scripts

--- GitHubActions/build_sqlite_fdw.sh
+++ GitHubActions/build_sqlite_fdw.sh
@@ -4,8 +4,9 @@
 #
 # This script builds sqlite_fdw in PostgreSQL source tree.
 #
-# Usage: ./build_sqlite_fdw.sh pg_version
+# Usage: ./build_sqlite_fdw.sh pg_version [configure_options]
 #     pg_version is a PostgreSQL version like 16.0 to be built in.
+#     configure_options are a list of option for sqlite_fdw compiler.
 #
 # Requirements
 # - the source code of sqlite_fdw is available by git clone.
@@ -14,8 +15,21 @@
 ################################################################################
 
 VERSION=$1
+
+CONFIGURE_OPTIONS=""
+
+while (( "$#" )); do
+  CONFIGURE_OPTIONS="$CONFIGURE_OPTIONS $1"
+  shift
+done
+
 mkdir -p ./workdir/postgresql-${VERSION}/contrib/sqlite_fdw
 tar zxvf ./sqlite_fdw.tar.gz -C ./workdir/postgresql-${VERSION}/contrib/sqlite_fdw/
 cd ./workdir/postgresql-${VERSION}/contrib/sqlite_fdw
 export LD_LIBRARY_PATH=/usr/local/lib/:$LD_LIBRARY_PATH
-make
+
+if [ -z "$CONFIGURE_OPTIONS" ]; then
+  make
+else
+  make $CONFIGURE_OPTIONS
+fi
