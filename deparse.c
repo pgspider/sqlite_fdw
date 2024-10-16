55408d54d50860f4925e4a2ed4ffec95aa18c9d6
Михаил <aksenov.mihail@bk.ru>
2024-10-16 20:56:03 +0300

    Fix code style

--- deparse.c
+++ deparse.c
@@ -2917,11 +2917,12 @@ sqlite_deparse_const(Const *node, deparse_expr_cxt *context, int showtype)
 									errhint("Data type: \"%s\" ", pg_dataTypeName),
 									errcontext("Value: %s", extval)));
 				}
-
-				/* common branch PostGIS constants, deparsable as a text data */
-				extval = OidOutputFunctionCall(typoutput, node->constvalue);
-				sqlite_deparse_string_literal(buf, extval);
-				break;
+				else
+				{
+					/* common branch PostGIS constants, deparsable as a text data */
+					extval = OidOutputFunctionCall(typoutput, node->constvalue);
+					sqlite_deparse_string_literal(buf, extval);
+				}
 			}
 	}
 }
