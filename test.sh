eaade87827cafedf70227a2367120375d1d041ed
Михаил <aksenov.mihail@bk.ru>
2024-12-13 10:05:57 +0300

    Add tests example

--- test.sh
+++ test.sh
@@ -10,8 +10,8 @@ sqlite3 "$testdir/core.db" < sql/init_data/init_core.sql;
 sqlite3 "$testdir/common.db" < sql/init_data/init.sql;
 sqlite3 "$testdir/selectfunc.db" < sql/init_data/init_selectfunc.sql;
 
-#sed -i 's/REGRESS =.*/REGRESS = extra\/sqlite_fdw_post types\/bitstring types\/bool types\/float4 types\/float8 types\/int4 types\/int8 types\/macaddr types\/macaddr8 types\/numeric types\/out_of_range types\/timestamp types\/uuid extra\/join extra\/limit extra\/aggregates extra\/prepare extra\/select_having extra\/select extra\/insert extra\/update extra\/encodings sqlite_fdw type aggregate selectfunc /' Makefile
-
+#export REGRESS=' YOUR TESTS HERE';
 make clean $1;
 make $1;
 make check $1 | tee make_check.out;
+export REGRESS=;
